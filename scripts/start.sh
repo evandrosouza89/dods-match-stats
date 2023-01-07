302bad0952f599c0996b0c21a3250f783857fce2
Evandro Souza <evandrosouza@cloud.upwork.com>
2023-01-07 20:10:56 -0300

    * Tweaking docker-compose

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,14 +5,30 @@ DMS_DB_PORT=3306
 DMS_DB_USR="secret"
 DMS_DB_PW="secret"
 DMS_DB_SCH="dods-match-stats"
-
 DMS_NETWORK="dms-network"
+
+function getContainerHealth {
+  docker inspect --format "{{.State.Health.Status}}" $1
+}
+
+function waitContainer {
+  while STATUS=$(getContainerHealth "$1"); [ "$STATUS" != "healthy" ]; do
+    if [ "$STATUS" == "unhealthy" ]; then
+      echo "Failed!"
+      exit 255
+    fi
+    printf .
+    lf=$'\n'
+    sleep 1
+  done
+  printf '%s' "$lf"
+}
 
 docker network create "$DMS_NETWORK"
 
-docker run --name "$DMS_DB_URL" -P -p "$DMS_DB_PORT":"$DMS_DB_PORT" -d -e MYSQL_ROOT_PASSWORD="$DMS_DB_PW" -e MYSQL_USER="$DMS_DB_USR" -e MYSQL_PASSWORD="$DMS_DB_PW" -e MYSQL_DATABASE="$DMS_DB_SCH" --network="$DMS_NETWORK" mysql:8
+docker run --name "$DMS_DB_URL" -P -p "$DMS_DB_PORT":"$DMS_DB_PORT" -d -e MYSQL_ROOT_PASSWORD="$DMS_DB_PW" -e MYSQL_USER="$DMS_DB_USR" -e MYSQL_PASSWORD="$DMS_DB_PW" -e MYSQL_DATABASE="$DMS_DB_SCH" --network="$DMS_NETWORK" --health-cmd='mysqladmin ping --silent' mysql:8
 
-sleep 10
+waitContainer mysql
 
 FILE_NAME='servers.txt'
 
